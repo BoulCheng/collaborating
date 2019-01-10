e31822928f2b3797b8490a911b471dfa0fab8542
BoulCheng <1241292849@qq.com>
2019-01-10 19:33:43 +0800

    first

---
+++ src/main/java/com/zlb/First.java
@@ -0,0 +1,13 @@
+package com.zlb;
+
+/**
+ * @author Yuanming Tao
+ * Created on 2019/1/10
+ * Description
+ */
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println("first");
+    }
+}
